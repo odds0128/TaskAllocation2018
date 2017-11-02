6cecf3dfc39fb55065fdc9cf5ad5f3e843b2f460
odds0128 <f.r_my-paaaace@docomo.ne.jp>
2017-11-02 17:45:56 +0900

    距離優先のみ実装終了

--- TaskManager.java
+++
@@ -1,9 +0,0 @@
-public class TaskManager {
-
-    public static class SetParam {
-        public static final int maxTask  =   6;
-        public static final int maxAgent = 500;
-        public static final int row      =  20;
-        public static final int column   =  25;
-    }
-}
