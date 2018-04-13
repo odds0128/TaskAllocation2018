e06c9b7813f409cea8c9893371d71df01103aa4e
odds0128 <f.r_my-paaaace@docomo.ne.jp>
2018-04-13 17:41:34 +0900

    出力ファイル名修正

--- OutPut.java
+++ OutPut.java
@@ -187,7 +187,7 @@ public class OutPut implements SetParam {
         String fileName = st.getClass().getName();
         try {
             String currentPath = System.getProperty("user.dir");
-            fw = new FileWriter(currentPath + "/out/results/" + fileName + ", λ=" + ADDITIONAL_TASK_NUM + ".csv", false);
+            fw = new FileWriter(currentPath + "/out/results/" + fileName + ", λ=" + ADDITIONAL_TASK_NUM/TASK_ADDITION_SPAN + ".csv", false);
             bw = new BufferedWriter(fw);
             pw = new PrintWriter(bw);
 
@@ -817,7 +817,7 @@ public class OutPut implements SetParam {
         String fileName = st.getClass().getName();
         try {
             String currentPath = System.getProperty("user.dir");
-            fw = new FileWriter(currentPath + "/out/results/rel" + fileName + ", λ=" + ADDITIONAL_TASK_NUM + ".csv", false);
+            fw = new FileWriter(currentPath + "/out/results/rel" + fileName + ", λ=" + ADDITIONAL_TASK_NUM/TASK_ADDITION_SPAN + ".csv", false);
             bw = new BufferedWriter(fw);
             pw = new PrintWriter(bw);
             // 列番号入れる部分
@@ -903,7 +903,7 @@ public class OutPut implements SetParam {
 
     private String setPath( String dir_name, String file_name ){
         String currentPath = System.getProperty("user.dir");
-        String outputFilePath = currentPath + "/out/" + dir_name + "/" + file_name + ",λ=" + ADDITIONAL_TASK_NUM + ".xlsx";
+        String outputFilePath = currentPath + "/out/" + dir_name + "/" + file_name + ",λ=" + ADDITIONAL_TASK_NUM/TASK_ADDITION_SPAN + ".xlsx";
         return outputFilePath;
     }
 
