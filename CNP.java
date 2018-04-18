c7adc49d6cf37c9aec5706d5757a63defdf20258
odds0128 <f.r_my-paaaace@docomo.ne.jp>
2018-04-18 14:02:25 +0900

    報酬優先実装&CNP実装開始

--- CNP.java
+++ CNP.java
@@ -1,3 +1,26 @@
-public class CNP {
+import java.util.List;
+
+public class CNP implements Strategy, SetParam{
+    public void actAsLeader(Agent agent){
+
+    }
+    public void actAsMember(Agent agent){
+
+    }
+    public List<Agent> selectMembers(Agent agent, List<SubTask> subtasks){
+        return null;
+    }
+    public Agent selectLeader(Agent agent, List<Message> messages){
+        return agent;
+    }
+
+    public void checkMessages(Agent self){
+
+    }
+    public void clearStrategy(){
+
+    }
+
+
 
 }
