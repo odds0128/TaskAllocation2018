b17503a3b171669b87f930eebd0624d2a167f717
odds0128 <f.r_my-paaaace@docomo.ne.jp>
2018-05-11 22:14:14 +0900

    PM2役割固定の，片方の役割だけ互恵性を無くした手法実装

--- Manager.java
+++ Manager.java
@@ -10,8 +10,11 @@ import java.util.*;
 public class Manager implements SetParam {
 //    private static Strategy strategy = new ProposedMethodForSingapore();
 //    private static Strategy strategy = new PM2();
-//    private static Strategy strategy = new PM2withRoleFixed();      // ICA1028における提案手法
-    private static Strategy strategy = new PM2withoutReciprocity();  // ICA2018における比較手法1
+//    private static Strategy strategy = new PM2withRoleFixed();      // ICA2018における提案手法
+//    private static Strategy strategy = new PM2withoutReciprocity();  // ICA2018における比較手法1
+//private static Strategy strategy = new PM2withoutReciprocalLeaderWithRoleFixed();      // ICA2018における提案手法
+    private static Strategy strategy = new PM2withoutReciprocalMemberWithRoleFixed();      // ICA2018における提案手法
+//
 //    private static Strategy strategy = new PMwithRoleFixed();
 //    private static Strategy strategy = new PMwithRationalOnly();
 //    private static Strategy strategy = new PMwithoutRoleRenewal();
