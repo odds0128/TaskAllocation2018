e09ddf5b24a1930d72e90ed4bcf2de3d574a5a2c
Ryoya Funato <fff.100necdt@asagi.waseda.jp>
2019-07-09 19:54:25 +0900

    clean: Extract the part that initiate agents from Manager class.
    ManagerクラスからAgentの初期化に関わる部分を抽出して独立したクラスを作成．
    
    ※ タグ
    ・fix：バグ修正
    ・add：新規（ファイル）機能追加
    ・update : 機能修正．(バグではない)
    ・remove : 削除(ファイル)
    
    ・hotfix：クリティカルなバグ修正
    ・change：仕様変更
    ・clean：整理（リファクタリング等）
    ・disable：無効化（コメントアウト等）
    ・remove：削除（ファイル）
    ・upgrade：バージョンアップ
    ・revert：変更取り消し

---
+++ test/main/research/ManagerTest.java
@@ -0,0 +1,12 @@
+package main.research;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ManagerTest {
+
+    @Test
+    void generateAgents() {
+    }
+}
---
+++ test/main/research/strategy/PM2Test.java
@@ -0,0 +1,7 @@
+package main.research.strategy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PM2Test {
+
+}
