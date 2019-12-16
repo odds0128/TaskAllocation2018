fd5783b4ece8b6467f9c55083c08b5a21ce97d4f
Ryoya Funato <fff.100necdt@asagi.waseda.jp>
2019-12-16 17:44:33 +0900

    clean: Divide nextPhase method of State class into each role State class

--- src/main/research/agent/strategy/State.java
+++ src/main/research/agent/strategy/State.java
@@ -13,41 +13,20 @@ import static main.research.SetParam.Phase.*;
 import static main.research.SetParam.Role.JONE_DOE;
 import static main.research.SetParam.Role.MEMBER;
 
-public interface Strategy {
+public abstract class Strategy {
 	int time_observing_team_formation_ = AgentManager.time_observing_team_formation_;
 
-	static void proceedToNextPhase( Agent ag ) {
-		switch ( ag.phase ) {
-			case WAIT_FOR_SOLICITATION:
-				ag.phase = WAIT_FOR_SUBTASK;
-				break;
-			case SOLICIT:
-				ag.phase = FORM_TEAM;
-				break;
-			case WAIT_FOR_SUBTASK:
-				ag.phase = EXECUTE_SUBTASK;
-				break;
-			case FORM_TEAM:
-				ag.phase = SELECT_ROLE;
-				ag.role = JONE_DOE;
-				break;
-			case EXECUTE_SUBTASK:
-				ag.phase = ag.ms.mySubtaskQueue.isEmpty() ? SELECT_ROLE : EXECUTE_SUBTASK;
-				ag.role = ag.ms.mySubtaskQueue.isEmpty() ? JONE_DOE : MEMBER;
-				break;
-		}
-		ag.validatedTicks = getCurrentTime();
-	}
+	protected abstract void nextPhase(Agent ag);
 
-	default boolean withinTimeWindow() {
+	protected boolean withinTimeWindow() {
 		return Agent._coalition_check_end_time - getCurrentTime() < time_observing_team_formation_;
 	}
 
-	default void evaporateDE( List< AgentDePair > pairList ) {
+	void evaporateDE( List< AgentDePair > pairList ) {
 		for ( AgentDePair pair: pairList ) pair.evaporate();
 	}
 
-	default AgentDePair getPairByAgent( Agent target, List< AgentDePair > pairList ) {
+	protected AgentDePair getPairByAgent( Agent target, List< AgentDePair > pairList ) {
 		for ( AgentDePair pair: pairList ) {
 			if ( pair.getAgent().equals( target ) ) return pair;
 		}
